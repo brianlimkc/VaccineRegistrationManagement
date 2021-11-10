f4685cad9215323f2baac976f5bcaa610e6c01a5
BrianLim80 <brian.lim.kc@gmail.com>
2021-11-10 16:24:51 +0800

    Update Testing.java

--- tests/test files/Testing.java
+++ tests/test files/Testing.java
@@ -1,22 +1,15 @@
 package vaccineRegistrationTest;
 
 import java.io.FileWriter;
-
 import java.text.SimpleDateFormat;  
 import java.util.Date;  
-
 import java.util.ArrayList;
-import java.util.List;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Testing 
